4b30806e12b6b26feab737cdfadae7e96a2c3eb6
Naranjo <jesusinf2510@gmail.com>
2020-12-13 22:25:09 -0600

    Agregado la clase java para cifrar datos

--- src/java/config/Conexion.java
+++ src/java/config/Conexion.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 public class Conexion {
     public Connection getConexion(){
         try {
-            Connection conexion = DriverManager.getConnection("jdbc:mysql://172.16.42.78:3306/serviciomedico", "Naranjo", "#r%^OJy#Eo8d");
+            Connection conexion = DriverManager.getConnection("jdbc:mysql://172.16.42.78:3306/serviciomedico?serverTimezone=UTC", "Naranjo", "#r%^OJy#Eo8d");
             return conexion;
         } catch (SQLException e) {
             System.out.println(e.toString());
